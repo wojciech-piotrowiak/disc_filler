4d3c4fbc1485388bd142a127815e0b4c9214a695
Wojciech Piotrowiak <wojciech.piotrowiak@gmail.com>
2016-04-30 15:03:53 +0200

    refactor

--- src/main/java/eu/wojciechpiotrowiak/Filler.java
+++ src/main/java/eu/wojciechpiotrowiak/Filler.java
@@ -6,6 +6,8 @@ import me.tongfei.progressbar.ProgressBar;
 import java.io.*;
 
 public class Filler {
+    int fileSize = 2_400_000;
+
     public void fillDirectory(String path) throws IOException {
         File file = new File(path);
         if (file.isDirectory()) {
@@ -31,37 +33,40 @@ public class Filler {
             return;
         }
 
-        ProgressBar pb = new ProgressBar("Test", 100);
+        ProgressBar pb = new ProgressBar("Filling data", 100);
         pb.start();
-        int oneMB = 1_000_000;
-        while (sizeInBytes > oneMB) {
-            sizeInBytes -= oneMB;
-            saveBytesIntoFile(directory, oneMB);
+        while (sizeInBytes > fileSize) {
+            sizeInBytes -= fileSize;
+            saveBytesIntoFile(directory, fileSize);
             pb.step();
         }
-        int leftover = (int) (oneMB - sizeInBytes);
+        long freeSpace = new File("").getFreeSpace();
+        int leftover = (int) (fileSize - sizeInBytes);
+        if (leftover > freeSpace) {
+            leftover = (int) freeSpace;
+        }
         if (leftover > 0) {
             saveBytesIntoFile(directory, leftover);
         }
         pb.stop();
     }
 
-    private void saveBytesIntoFile(String directory, int oneMB) throws IOException {
+    private void saveBytesIntoFile(String directory, int totalLength) throws IOException {
         String name = directory + File.separator + System.nanoTime();
         try (
                 FileOutputStream outputStream = new FileOutputStream(name);
                 BufferedOutputStream out = new BufferedOutputStream(outputStream)) {
 
-            writeBufferIntoStream(oneMB, out);
+            writeBufferIntoStream(totalLength, out);
             out.flush();
         }
     }
 
-    private void writeBufferIntoStream(int length, BufferedOutputStream out) throws IOException {
-        byte[] buffer = new byte[length];
-        for (int i = 0; i < length; i++) {
+    private void writeBufferIntoStream(int totalLength, BufferedOutputStream out) throws IOException {
+        byte[] buffer = new byte[totalLength];
+        for (int i = 0; i < totalLength; i++) {
             buffer[i] = 1;
         }
-        out.write(buffer, 0, length);
+        out.write(buffer, 0, totalLength);
     }
 }
